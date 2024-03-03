ffe7817291d7d6e5bee432506746ad9be7a41c6a
joergbeicht <joergbeicht@confessio-management.de>
2024-03-03 08:36:13 +0100

    Observable T Generics siehe showLoaderUntilCompleted

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {Course, sortCoursesBySeqNo} from '../model/course';
-import {interval, noop, Observable, of, throwError, timer} from 'rxjs';
-import {catchError, delay, delayWhen, filter, finalize, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
-import {HttpClient} from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Course, sortCoursesBySeqNo } from '../model/course';
+import { interval, noop, Observable, of, throwError, timer } from 'rxjs';
+import { catchError, delay, delayWhen, filter, finalize, map, retryWhen, shareReplay, tap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 import { CoursesService } from '../services/courses.service';
 import { LoadingService } from '../loading/loading.service';
 
@@ -22,7 +22,7 @@ export class HomeComponent implements OnInit {
     public loadingService: LoadingService,
     private coursesService: CoursesService,
     private http: HttpClient
-  ) {}
+  ) { }
 
   ngOnInit() {
     this.reloadCourses();
@@ -32,35 +32,45 @@ export class HomeComponent implements OnInit {
 
     console.log('reloadCourses', event);
 
-    this.loadingService.loadingOn();
+    // vorher
+    // this.loadingService.loadingOn();
 
+    // const courses$ = this.coursesService.loadAllCourses()
+    //   .pipe(
+    //     map(courses => courses.sort(sortCoursesBySeqNo)),
+    //     finalize(() => this.loadingService.loadingOff())
+    //   )
+
+    // this.beginnerCourses$ = courses$.pipe(
+    //   map(courses => courses.filter(course => course.category == 'BEGINNER'))
+    // );
+
+    // this.advancedCourses$ = courses$.pipe(
+    //   map(courses => courses.filter(course => course.category == 'ADVANCED'))
+    // );
+
+    // nachher
     const courses$ = this.coursesService.loadAllCourses()
-    .pipe(
-      map(courses => courses.sort(sortCoursesBySeqNo)),
-      finalize(() => this.loadingService.loadingOff())
-    )
+      .pipe(
+        map(courses => courses.sort(sortCoursesBySeqNo))
+      )
 
-    // courses$.subscribe(val => console.log('val', val));
+    // showLoaderUntilCompleted<Course[]> bedeutet,
+    // Das Observable emits ein Array of courses
+    // loadCourses$ ist damit typsiert und der weitere Code ist type save, obwohl
+    // showLoaderUntilCompleted eine generische Methode ist
+    const loadCourses$ = this.loadingService.showLoaderUntilCompleted<Course[]>(courses$);
 
-    this.beginnerCourses$ = courses$.pipe(
+    this.beginnerCourses$ = loadCourses$.pipe(
       map(courses => courses.filter(course => course.category == 'BEGINNER'))
     );
 
-    this.advancedCourses$ = courses$.pipe(
+    this.advancedCourses$ = loadCourses$.pipe(
       map(courses => courses.filter(course => course.category == 'ADVANCED'))
     );
 
-    // this.http.get('/api/courses')
-    //   .subscribe(
-    //     res => {
 
-    //       const courses: Course[] = res["payload"].sort(sortCoursesBySeqNo);
 
-    //       this.beginnerCourses = courses.filter(course => course.category == "BEGINNER");
-
-    //       this.advancedCourses = courses.filter(course => course.category == "ADVANCED");
-
-    //     });
   }
 
 }
--- src/app/loading/loading.service.ts
+++ src/app/loading/loading.service.ts
@@ -15,11 +15,19 @@ export class LoadingService {
         console.log("Loading service created ...");        
     }
 
+    // https://entwickler.de/typescript/grundkurs-typescript-001
+    // siehe Type T suche nach Klassen und Methoden parametrisieren mit Generics
     showLoaderUntilCompleted<T>(obs$: Observable<T>): Observable<T> {
+        // of erstellt ein default Observable das mit null initialisiert wird
+        // nachdem das Observable den null Wert empfangen hat, startet die  
+        // pipe und tap usw.
         return of(null)
             .pipe(
                 tap(() => this.loadingOn()),
+                // Das Observable soll aber nicht null von init zurückgeben,
+                // sondern das reingesteckte Observable
                 concatMap(() => obs$),
+                // erst enn das Input Observable obs$: Observable<T> fertig ist soll finalize erfolgen
                 finalize(() => this.loadingOff())
             );
     }
